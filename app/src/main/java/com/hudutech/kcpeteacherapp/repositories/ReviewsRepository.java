a524642b4b2c1fb8dc4babeb162adf47bd328116
Njeru Cyrus <njerucyrus5@gmail.com>
2020-03-24 17:34:41 +0300

    updated reviews repository to include fetch review method

--- app/src/main/java/com/hudutech/kcpeteacherapp/repositories/ReviewsRepository.java
+++ app/src/main/java/com/hudutech/kcpeteacherapp/repositories/ReviewsRepository.java
@@ -1,16 +1,61 @@
 package com.hudutech.kcpeteacherapp.repositories;
 
 import android.app.Application;
+import android.util.Log;
+import android.view.View;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.hudutech.kcpeteacherapp.models.StarReview;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewsRepository {
+    private static final String TAG = "ReviewsRepository";
     private static ReviewsRepository instance;
+    private final CollectionReference mReviewsRef = FirebaseFirestore.getInstance().collection("teacher_ratings");
     private Application mApplication;
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>();
+
     public static ReviewsRepository getInstance(Application application) {
         if (instance == null) {
             instance = new ReviewsRepository();
             instance.mApplication = application;
         }
         return instance;
+    }
+
+    public MutableLiveData<List<StarReview>> fetchReviews(String userId) {
+        final List<StarReview> reviews = new ArrayList<>();
+        MutableLiveData<List<StarReview>> reviewsLiveData = new MutableLiveData<>();
+        isLoading.postValue(true);
+        mReviewsRef.whereEqualTo("reviewedUserId", userId)
+                .addSnapshotListener((queryDocumentSnapshots, e) -> {
+                    isLoading.postValue(false);
+                    if (e != null) {
+                        Log.e(TAG, "fetchReviews: ", e);
+                        return;
+                    }
+                    reviews.clear();
+                    if (queryDocumentSnapshots != null) {
+                        for (DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()) {
+                            StarReview review = snapshot.toObject(StarReview.class);
+                            if (review != null) {
+                                reviews.add(review);
+
+                            }
+                        }
+
+                        reviewsLiveData.postValue(reviews);
+
+                    }
+
+                });
+        return reviewsLiveData;
     }
 
 
