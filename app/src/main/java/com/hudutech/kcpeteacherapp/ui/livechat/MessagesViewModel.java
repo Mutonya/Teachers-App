b61e1fa983dfeeb354e03c65389aa092c35504c5
Njeru Cyrus <njerucyrus5@gmail.com>
2020-03-25 22:39:52 +0300

    added messagesviewmodel

---
+++ app/src/main/java/com/hudutech/kcpeteacherapp/ui/livechat/MessagesViewModel.java
@@ -0,0 +1,12 @@
+package com.hudutech.kcpeteacherapp.ui.livechat;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MessagesViewModel extends AndroidViewModel {
+    public MessagesViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
